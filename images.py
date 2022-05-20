dacfaaa936c465f6877061457647c499f5120dfc
Uskompuf <22492406+Uskompuf@users.noreply.github.com>
2022-05-20 17:46:05 +1000

    update archistructure

---
+++ images.py
@@ -0,0 +1,44 @@
+from mimetypes import guess_extension
+import os
+import requests
+import json
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+raw_file = open("brands/raw.json", "r")
+brands = json.load(raw_file)
+raw_file.close()
+
+changed_images = 0
+
+for brand in brands:
+    try:
+        r = requests.get(brands[brand]['logoUrl'], headers=headers)
+        file_extension = guess_extension(r.headers['Content-Type'].partition(';')[0].strip())
+        if file_extension == None and r.headers['Content-Type'] == 'image/webp':
+            file_extension = '.webp'
+        if file_extension == '.jpe':
+            file_extension = '.jpg'
+        if file_extension == '.htm' or file_extension == '.html':
+            continue
+        path = 'brands/logos/' + brand + file_extension
+        oldFile = ""
+        for file in os.listdir('brands/logos/'):
+            if brand in file:
+                oldFile = 'brands/logos/' + file     
+        with open(path, 'wb') as f:
+            f.write(r.content)
+        if oldFile != "" and oldFile != path:
+            os.remove(oldFile)
+        if oldFile != path:
+            changed_images += 1
+
+    except Exception as e:
+        print(e)
+
+stats = {}
+stats['changed_images'] = changed_images
+
+raw_file = open("stats.json", "w")
+json.dump(stats, raw_file, indent = 4)
+raw_file.close()
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from mimetypes import guess_extension
-import os
 import requests
 import json
-
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 brands = {}
 
@@ -11,15 +7,9 @@ r = requests.get('https://api.cdr.gov.au/cdr-register/v1/banking/register')
 
 available_brands = r.json()
 
-try:
-    raw_file = open("brands/raw.json", "w")
-    
-    json.dump(available_brands, raw_file, indent = 4)
-    
-    raw_file.close()
-
-except Exception as e:
-    print(e)
+raw_file = open("brands/raw.json", "w")
+json.dump(available_brands, raw_file, indent = 4)
+raw_file.close()
 
 available_brands = available_brands['registerDetails']
 
@@ -42,52 +32,7 @@ for entity in available_brands:
             if 'brandDescription' in brand:
                 brands[brand['brandRef']]['brandDescription'] = brand['brandDescription']
 
-try:
-    brands_file = open("brands/brands.json", "w")
-    
-    json.dump(brands, brands_file, indent = 4)
-    
-    brands_file.close()
 
-except Exception as e:
-    print(e)
-
-changed_images = 0
-
-for brand in brands:
-    try:
-        r = requests.get(brands[brand]['logoUrl'], headers=headers)
-        file_extension = guess_extension(r.headers['Content-Type'].partition(';')[0].strip())
-        if file_extension == None and r.headers['Content-Type'] == 'image/webp':
-            file_extension = '.webp'
-        if file_extension == '.jpe':
-            file_extension = '.jpg'
-        if file_extension == '.htm' or file_extension == '.html':
-            continue
-        path = 'brands/logos/' + brand + file_extension
-        oldFile = ""
-        for file in os.listdir('brands/logos/'):
-            if brand in file:
-                oldFile = 'brands/logos/' + file     
-        with open(path, 'wb') as f:
-            f.write(r.content)
-        if oldFile != "" and oldFile != path:
-            os.remove(oldFile)
-        if oldFile != path:
-            changed_images += 1
-
-    except Exception as e:
-        print(e)
-
-stats = {}
-stats['changed_images'] = changed_images
-
-try:
-    raw_file = open("stats.json", "w")
-    
-    json.dump(stats, raw_file, indent = 4)
-    
-    raw_file.close()
-
-except Exception as e:
-    print(e)
+brands_file = open("brands/brands.json", "w")
+json.dump(brands, brands_file, indent = 4)
+brands_file.close()
