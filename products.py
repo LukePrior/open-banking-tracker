8d693bc268ffe3652d7d33df0a15da73bf4d2e1f
Luke Prior <22492406+Uskompuf@users.noreply.github.com>
2022-10-04 16:26:55 +1100

    feat: multithread

--- products.py
+++ products.py
@@ -2,8 +2,11 @@ from mimetypes import guess_extension
 import os
 import requests
 import json
+from multiprocessing.pool import ThreadPool as Pool
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36', 'x-v': '3'}
+
+brand_size = 10
 
 def get_data(url):
     r = requests.get(url, headers=headers)
@@ -21,6 +24,7 @@ def get_data(url):
 
     return response
 
+
 # https://stackoverflow.com/a/25851972/9389353
 def ordered(obj):
     if isinstance(obj, dict):
@@ -30,36 +34,24 @@ def ordered(obj):
     else:
         return obj
 
-raw_file = open("brands/brands.json", "r")
-brands = json.load(raw_file)
-raw_file.close()
 
-changed_products = 0
-
-for brand in brands:
+def process_brand(brand):
     try:
         response = get_data(brands[brand]['publicBaseUri'].rstrip('/') + "/cds-au/v1/banking/products")
 
-        flag = False
         skip_update = False
 
         try:
             for file in os.listdir('brands/products/'):
                 if file == (brand + ".json"):
-                    flag = True
                     raw_file = open("brands/products/"+file, "r")
                     response_compare = json.load(raw_file)
                     raw_file.close()
-                    if ordered(response) != ordered(response_compare):
-                        changed_products += 1
-                    else:
+                    if ordered(response) == ordered(response_compare):
                         skip_update = True
 
         except Exception as e:
             print(e)
-
-        if flag == False:
-            changed_products += 1
 
         path = 'brands/products/' + brand + ".json"
 
@@ -73,9 +65,13 @@ for brand in brands:
     except Exception as e:
         print(e)
 
-stats = {}
-stats['changed_files'] = changed_products
-
-raw_file = open("stats.json", "w")
-json.dump(stats, raw_file, indent = 4)
+raw_file = open("brands/brands.json", "r")
+brands = json.load(raw_file)
 raw_file.close()
+
+brand_pool = Pool(brand_size)
+for brand in brands:
+    brand_pool.apply_async(process_brand, (brand,))
+
+brand_pool.close()
+brand_pool.join()
