58e44cd80b28c8f9f84a19aa372b356385623a9c
Luke Prior <22492406+Uskompuf@users.noreply.github.com>
2023-10-02 19:37:32 +1100

    bump version

--- product.py
+++ product.py
@@ -4,7 +4,7 @@ import requests
 import json
 from multiprocessing.pool import ThreadPool as Pool
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36', 'x-v': '3'}
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36', 'x-v': '4'}
 
 brand_size = 10
 product_size = 5
